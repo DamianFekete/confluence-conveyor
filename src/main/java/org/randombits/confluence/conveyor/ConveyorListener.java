fd7f8a43673a5c7bef18c7a2bb7df5e68bd3a050
dpeterson@2c54a935-e501-0410-bc05-97a93f6bca70 <dpeterson@2c54a935-e501-0410-bc05-97a93f6bca70>
2007-08-28 01:46:18 +0000

    * [CNVR] Removed unnecessary event listening.
    --HG--
    extra : convert_revision : svn%3A2c54a935-e501-0410-bc05-97a93f6bca70/LIBCNV/trunk%40130916

--- src/main/java/org/randombits/confluence/conveyor/ConveyorListener.java
+++ src/main/java/org/randombits/confluence/conveyor/ConveyorListener.java
@@ -1,24 +1,18 @@
 package org.randombits.confluence.conveyor;
 
-import com.atlassian.confluence.event.events.plugin.PluginDisableEvent;
-import com.atlassian.confluence.event.events.plugin.PluginEnableEvent;
-import com.atlassian.confluence.event.events.plugin.PluginInstallEvent;
-import com.atlassian.confluence.event.events.plugin.PluginUninstallEvent;
 import com.atlassian.event.Event;
 import com.atlassian.event.EventListener;
 import com.atlassian.plugin.StateAware;
 
 public class ConveyorListener implements EventListener, StateAware {
 
-    private static final Class[] HANDLED_CLASSES = new Class[]{PluginInstallEvent.class,
-            PluginUninstallEvent.class, PluginEnableEvent.class, PluginDisableEvent.class};
+    private static final Class[] HANDLED_CLASSES = new Class[]{};
 
     public Class[] getHandledEventClasses() {
         return HANDLED_CLASSES;
     }
 
     public void handleEvent( Event event ) {
-        ConveyorAssistant.getInstance().reload();
     }
 
     public void disabled() {
