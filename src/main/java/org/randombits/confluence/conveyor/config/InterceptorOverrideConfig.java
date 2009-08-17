787e6855a7cb15b727d1ecf04c091b8ec6076fa8
dpeterson@2c54a935-e501-0410-bc05-97a93f6bca70 <dpeterson@2c54a935-e501-0410-bc05-97a93f6bca70>
2009-08-17 08:16:14 +0000

    * Added the InterceptorOverrideConfig class * Updated to 1.0.0-SNAPSHOT.
    --HG--
    extra : convert_revision : svn%3A2c54a935-e501-0410-bc05-97a93f6bca70/LIBCNV/trunk%40131339

---
+++ src/main/java/org/randombits/confluence/conveyor/config/InterceptorOverrideConfig.java
@@ -0,0 +1,50 @@
+package org.randombits.confluence.conveyor.config;
+
+import java.util.Map;
+
+import com.opensymphony.xwork.config.entities.InterceptorConfig;
+
+public class InterceptorOverrideConfig extends InterceptorConfig {
+
+    private InterceptorConfig overriddenInterceptor;
+
+    public InterceptorOverrideConfig() {
+        super();
+    }
+
+    public InterceptorOverrideConfig( InterceptorConfig overriddenInterceptor, boolean copySettings, String name,
+            Class clazz, Map params ) {
+        super( name, clazz, params );
+        setOverriddenInterceptor( overriddenInterceptor, copySettings );
+    }
+
+    public InterceptorOverrideConfig( InterceptorConfig overriddenInterceptor, boolean copySettings, String name,
+            String className, Map params ) {
+        super( name, className, params );
+        setOverriddenInterceptor( overriddenInterceptor, copySettings );
+    }
+
+    public InterceptorConfig getOverriddenInterceptor() {
+        return overriddenInterceptor;
+    }
+
+    private void setOverriddenInterceptor( InterceptorConfig overriddenInterceptor, boolean copySettings ) {
+        this.overriddenInterceptor = overriddenInterceptor;
+        if ( copySettings ) {
+            if ( this.getClassName() == null ) {
+                setClassName( overriddenInterceptor.getClassName() );
+            }
+
+            // Copy the new params over the old params.
+            Map<String, String> oldParams = ConveyorConfigurationProvider.copyParams( overriddenInterceptor
+                    .getParams() );
+            if ( oldParams != null ) {
+                Map<String, String> params = getParams();
+                if ( params != null )
+                    oldParams.putAll( params );
+                setParams( oldParams );
+            }
+        }
+    }
+
+}
